e3580086425ee7b26fe0ca9bdbbfc7ea28b278f7
David Dinh <dmdinh22@gmail.com>
2018-03-31 00:33:14 -0700

    prime number checker

--- Summation/src/summation/SumOfNumbers.java
+++ Summation/src/summation/SumOfNumbers.java
@@ -5,10 +5,19 @@ public class SumOfNumbers {
 		int sum = 0;
 		int number = 1;
 		
+		// while loop - as long as number is less than 101
+		// better when you don't know when your loop will end
 		while (number <= 100) {
 			sum = sum + number;
-			number = number + 1;
+			number = number + 2;
 		}
-		System.out.println("sum of numbers from 1 to 100 is " + sum);
+		System.out.println("while loop - sum of all odd numbers from 1 to 100 is " + sum);
+		
+		// for loop - if you know your loop will run a certain amount of times
+		int forSum = 0;
+		for (int i = 1; i <= 100; i++) {
+			forSum += i;
+		}
+		System.out.println("for loop - sum of all numbers from 1 to 100 is " + forSum);
 	}
 }
