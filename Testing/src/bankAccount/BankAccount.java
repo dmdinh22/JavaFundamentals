2e7a4f5f6cb005ee117321e48423ec412b803f54
David Dinh <dmdinh22@gmail.com>
2018-03-31 13:04:00 -0700

    jUnit test

---
+++ Testing/src/bankAccount/BankAccount.java
@@ -0,0 +1,11 @@
+package bankAccount;
+
+public class BankAccount {
+	double balance;
+	String accountOwner;
+	
+	public void deposit(double amount) {
+		
+	}
+}
+
---
+++ Testing/src/bankAccount/BankAccountTest.java
@@ -0,0 +1,14 @@
+package bankAccount;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class BankAccountTest {
+
+	@Test
+	void testDeposit() {
+		fail("Not yet implemented"); 
+	}
+
+}
