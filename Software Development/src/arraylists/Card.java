1d90e4905b4711270b7d4994e97e0e37d1dfe55e
David Dinh <dmdinh22@gmail.com>
2018-03-31 16:37:39 -0700

    arraylist ex with cards

---
+++ Software Development/src/arraylists/Card.java
@@ -0,0 +1,22 @@
+package arraylists;
+
+import java.util.*;
+
+public class Card {
+	char suit; //d, s, c, h
+	int rank; // J is 11, Q is 12, K is 13
+	
+	// creating a deck of cards
+	ArrayList<Card> deck = new ArrayList<Card>();
+	suits = new char[] {'d', 's', 'c', 'h'};
+	for (int i = 1; i <= 13; i++ ) {
+		Card c = new Card();
+		for (int j = 0; j <= 4; j++) {
+			c.suit = suits[j];
+			c.rank = i;
+			deck.add(c);
+		}
+	}
+}
+
+
